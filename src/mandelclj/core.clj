9167208db30af9635e59e867f4545b5f04752749
jibi <jibi@paranoici.org>
2013-01-09 13:45:13 +0100

    it seems to work

--- src/mandelclj/core.clj
+++ src/mandelclj/core.clj
@@ -25,10 +25,40 @@
 		(> (c_abs z) 2.0)))
 
 (defn mandelbrot-eval [c]
-	(letfn [(x-mandelbrot-eval [niter, z]
+	(letfn [(x-mandelbrot-eval [niter z]
 		(if (terminate-p niter z)
 			niter
-			(x-mandelbrot-eval (+ 1 niter) (mandelbrot-fun z c)))
+			(x-mandelbrot-eval (+ niter 1) (mandelbrot-fun z c)))
 	)]
+
 	(x-mandelbrot-eval 0 [0 0])))
 
+(defn yloop [count x y ystep ynsteps l]
+	(if (< count ynsteps)
+		(yloop (+ count 1) x (+ y ystep) ystep ynsteps
+			(conj l (mandelbrot-eval [y x])))
+	l))
+
+(defn xloop [count x xstep xnsteps ymin ystep ynsteps l]
+	(if (< count xnsteps)
+		(xloop (+ count 1) (+ x xstep) xstep xnsteps ymin ystep ynsteps
+			(conj l (yloop 0 x ymin ystep ynsteps [])))
+	l))
+
+(defn compute-mandelbrot-set [xmin xmax ymin ymax xnsteps ynsteps]
+	(let [xstep (/ (- xmax xmin) (- xnsteps 1))
+		ystep (/ (- ymax ymin) (- ynsteps 1))]
+		(xloop 0 xmin xstep xnsteps ymin ystep ynsteps [])
+	))
+
+(defn printm [x]
+	(if (= x max-iterations) (print "*") (print " ")))
+
+(defn printmln [l]
+	(dorun (map printm l))
+	(println ""))
+
+(defn plot-mandelbrot [nsteps]
+	(dorun (map printmln (compute-mandelbrot-set -1 1 -1.5 0.5 nsteps (* nsteps 2)))))
+
+(plot-mandelbrot 50)
