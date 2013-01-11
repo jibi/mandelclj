0ae476c15c00488272b72290e576757709220420
Bronsa <brobronsa@gmail.com>
2013-01-11 19:19:30 +0100

    massive restyle.

--- project.clj
+++ project.clj
@@ -1,2 +1,7 @@
 (defproject jibi/mandelclj "0.0.1"
-	:description "Mandelbrot set with clojure")
+  :description "Mandelbrot set with clojure"
+  :url "https://github.com/jibi/mandelclj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main mandelclj.core
+  :dependencies [[org.clojure/clojure "1.4.0"]])
